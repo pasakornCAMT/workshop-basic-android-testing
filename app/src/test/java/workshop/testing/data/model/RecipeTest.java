b570da3a412193f89d42166c5ff73720e5a1cb84
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-04-23 22:25:33 +0700

    read the list

--- app/src/test/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -1,7 +1,9 @@
 package workshop.testing.data.model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.*;
@@ -10,29 +12,28 @@ import static org.junit.Assert.*;
  * Created by CAMT on 17/4/2561.
  */
 public class RecipeTest {
+    InputStream inputStream;
+    Recipe recipe;
 
     @Test
-    public void readWater(){
+    public void readAll(){
         //Arrange
-        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
+        inputStream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
 
         //Action
-        Recipe recipe = Recipe.readFromStream(inputStream);
+        recipe = Recipe.readFromStream(inputStream);
 
         //Assert
         assertNotNull(recipe);
         assertEquals("water",recipe.id);
         assertEquals("Water",recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
-    }
 
-    @Test
-    public void readMixed(){
-        //Arange
-        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        //Arrange
+        inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
 
         //Action
-        Recipe recipe = Recipe.readFromStream(inputStream);
+        recipe = Recipe.readFromStream(inputStream);
 
         //Assert
         assertNotNull(recipe);
@@ -45,20 +46,28 @@ public class RecipeTest {
                 "1 cup water\n" +
                 "1 pine apple juice\n" +
                 "Mix all together and strain. Add large piece of ice.",recipe.description);
-    }
 
-    @Test
-    public void readNoId(){
         //Arrange
-        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/no_id.txt");
+        inputStream = RecipeTest.class.getResourceAsStream("/recipes/no_id.txt");
 
         //Action
-        Recipe recipe = Recipe.readFromStream(inputStream);
+        recipe = Recipe.readFromStream(inputStream);
 
         //Assert
         assertNotNull(recipe);
         assertEquals(null,recipe.id);
         assertEquals("Water",recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
+
+        //Arrange
+        inputStream = RecipeTest.class.getResourceAsStream("/recipes/empty.txt");
+
+        //Action
+        recipe = Recipe.readFromStream(inputStream);
+
+        //Assert
+        assertNotNull(recipe);
+        assertEquals("empty",recipe.id);
+        assertEquals("Empty",recipe.title);
     }
 }
